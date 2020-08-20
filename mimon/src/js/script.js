0f6a308158087e9e395b04431ad2c8be9ae3ef3b
ViacheslavOM <61595996+ViacheslavOM@users.noreply.github.com>
2020-08-20 12:42:41 +0200

    Add files via upload

---
+++ mimon/src/js/script.js
@@ -0,0 +1,12 @@
+$("#btn1").click(function() {
+    $(".news__menu_undermenu").toggle();
+});
+$("#btn2").click(function() {
+    $(".news__menu_undermenu-1").toggle();
+});
+$("#btn3").click(function() {
+    $(".news__menu_undermenu-2").toggle();
+});
+$("#btn4").click(function() {
+    $(".news__menu_undermenu-3").toggle();
+});
